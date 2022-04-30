7bc46917f66bdeec5c9f3a0c7c5ceb3a2207ba8e
MichaelSoquat <michael.soquat@gmx.de>
2022-04-30 08:23:35 +0200

    added votes

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,9 @@
 from django.urls import URLPattern, path
-from .views import index, umfrage_detail
+from .views import index, umfrage_detail, vote, results
 
 urlpatterns = [
-    path('', index),
-    path('abstimmung/<str:slug>', umfrage_detail)
+    path('', index, name='index'),
+    path('poll/<str:slug>', umfrage_detail, name='umfrage-detail'),
+    path('poll/<str:slug>/vote', vote, name='vote'),
+    path('poll/<str:slug>/results', results, name='results'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,32 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from .models import Poll
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.shortcuts import render, get_object_or_404
+
+from .models import Choice, Poll
 
 def index(request):
     
     context = {'umfragen': Poll.objects.all(), 'titel': "Umfragenseite"}
-    return render(request, "polls/index.html", context)
+    return render(request, 'polls/index.html', context)
 
 def umfrage_detail(request, slug):
-    umfrage = Poll.objects.get(slug=slug)
-    return HttpResponse('{0}'.format(umfrage.name))
+    umfrage = get_object_or_404(Poll, slug=slug)
+    context = {'umfrage': umfrage}
+    return render(request=request, template_name='polls/umfrage.html', context=context)
+
+def results(request, slug):     
+    umfrage = get_object_or_404(Poll, slug=slug)
+    context = {'umfrage': umfrage}
+    return render(request=request, template_name='polls/results.html', context=context)
+
+def vote(request, slug):
+    umfrage = get_object_or_404(Poll, slug=slug)
+    try:
+        ausgewaehlte_antwort = umfrage.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return HttpResponse("Fehler: Es wurde keine bzw. eine ungültige Antwort ausgewählt")
+    else:
+         ausgewaehlte_antwort.votes +=1
+         ausgewaehlte_antwort.save()
+         return HttpResponseRedirect(reverse('results', args=(umfrage.slug,)))
+
